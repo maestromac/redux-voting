c46f95868af0cf5b8a532ede65fedec01198a24a
Mac Siri <krairit.siri@gmail.com>
2016-11-16 18:53:46 -0500

    implement next function and test

--- test/core_spec.js
+++ test/core_spec.js
@@ -1,7 +1,6 @@
 import {expect} from 'chai';
 import {List, Map} from 'immutable';
-
-import {setEntries} from '../src/core';
+import {setEntries, next, vote} from '../src/core';
 
 
 describe('application logic', () => {
@@ -24,8 +23,67 @@ describe('application logic', () => {
         entries: List.of('Trainspotting', '28 Days Later')
       }));
     });
-
-
   });
+
+  describe('next', () => {
+    it('takes the next two entires under vote', () => {
+      const state = Map({
+        entries: List.of('Trainspotting', '28 Days Later', 'Sunshine')
+      });
+      const nextState = next(state);
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later')
+        }),
+        entries: List.of('Sunshine')
+      }));
+    });
+  });
+
+  describe('vote', () => {
+   it('creates a tally for the voted entry', () => {
+     const state = Map({
+       vote: Map({
+         pair: List.of('Trainspotting', '28 Days Later')
+       }),
+       entries: List()
+     });
+     const nextState = vote(state, 'Trainspotting');
+     expect(nextState).to.equal(Map({
+       vote: Map({
+         pair: List.of('Trainspotting', '28 Days Later'),
+         tally: Map({
+           'Trainspotting': 1
+         })
+       }),
+       entries: List()
+     }));
+   });
+
+   it('adds to existing tally for the voted entry', () => {
+     const state = Map({
+       vote: Map({
+         pair: List.of('Trainspotting', '28 Days Later'),
+         tally: Map({
+           'Trainspotting': 3,
+           '28 Days Later': 2
+         })
+       }),
+       entries: List()
+     });
+     const nextState = vote(state, 'Trainspotting');
+     expect(nextState).to.equal(Map({
+       vote: Map({
+         pair: List.of('Trainspotting', '28 Days Later'),
+         tally: Map({
+           'Trainspotting': 4,
+           '28 Days Later': 2
+         })
+       }),
+       entries: List()
+     }));
+   });
+
+ });
 
 });
