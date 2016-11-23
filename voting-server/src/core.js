6d5904a9b92a1412cfdbe9450b8543299f7b9f97
Mac Siri <krairit.siri@gmail.com>
2016-11-23 16:38:24 -0500

    complete reducer and reducer_spec

--- voting-server/src/core.js
+++ voting-server/src/core.js
@@ -1,21 +1,40 @@
 import {List, Map} from 'immutable';
 
+export const INITIAL_STATE = Map();
+
 export function setEntries(state, entries) {
   return state.set('entries', List(entries));
 }
 
-export function next(state) {
-  const entries = state.get('entries');
-  return state.merge({
-    vote: Map({pair: entries.take(2)}),
-    entries: entries.skip(2)
-  });
-}
-
-export function vote(state, entry) {
-  return state.updateIn(
-    ['vote', 'tally', entry],
+export function vote(voteState, entry) {
+  return voteState.updateIn(
+    ['tally', entry],
     0,
     tally => tally + 1
   );
+}
+
+function getWinners(vote) {
+  if (!vote) return [];
+  const [a, b] = vote.get('pair');
+  const aVotes = vote.getIn(['tally', a], 0);
+  const bVotes = vote.getIn(['tally', b], 0);
+  if      (aVotes > bVotes)  return [a];
+  else if (aVotes < bVotes)  return [b];
+  else                       return [a, b];
+}
+
+export function next(state) {
+  const entries = state.get('entries')
+                       .concat(getWinners(state.get('vote')));
+  if (entries.size === 1) {
+    return state.remove('vote')
+                .remove('entries')
+                .set('winner', entries.first());
+  } else {
+    return state.merge({
+      vote: Map({pair: entries.take(2)}),
+      entries: entries.skip(2)
+    });
+  }
 }
